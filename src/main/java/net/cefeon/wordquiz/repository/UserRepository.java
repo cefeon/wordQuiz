805e991a043a8266ed55b726391d6b40b0ccd9a8
cefeon <cefeon@outlook.com>
2021-12-16 12:39:10 +0100

    Add Spring Security basic HTTP authentication

---
+++ src/main/java/net/cefeon/wordquiz/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package net.cefeon.wordquiz.repository;
+
+import net.cefeon.wordquiz.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
---
+++ src/main/java/net/cefeon/wordquiz/service/AppUserDetailsService.java
@@ -0,0 +1,27 @@
+package net.cefeon.wordquiz.service;
+
+import net.cefeon.wordquiz.model.User;
+import net.cefeon.wordquiz.nonmodel.AppUserDetails;
+import net.cefeon.wordquiz.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+        return user.map(AppUserDetails::new).get();
+    }
+}
