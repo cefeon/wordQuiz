c7b5e3849ad1d26919f1a1cad7ceded491825f11
cefeon <cefeon@outlook.com>
2021-12-27 23:38:23 +0100

    Add Heroku updated configfiles

--- src/main/java/net/cefeon/wordquiz/WordQuizApplication.java
+++ src/main/java/net/cefeon/wordquiz/WordQuizApplication.java
@@ -2,10 +2,11 @@ package net.cefeon.wordquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class WordQuizApplication {
+public class WordQuizApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WordQuizApplication.class, args);
     }
